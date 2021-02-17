e68b62664e94b86a353e8ad0b4620e4188cd4380
Alessandro De Marco <lookatde.lad@gmail.com>
2021-02-17 17:38:42 +0100

    Scroll to top

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+var mybutton = document.getElementById("top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 var app = new Vue({
   el: '#app',
   data: {
