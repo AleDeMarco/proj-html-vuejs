5c2e867a508e2e6576ce4373867bdefe8b6e7a22
Alessandro De Marco <lookatde.lad@gmail.com>
2021-02-17 17:28:08 +0100

    End of the project

--- js/script.js
+++ js/script.js
@@ -105,7 +105,154 @@ var app = new Vue({
         name: 'Anastacia Smith',
         role: 'Partner'
       }
+    ],
+    counterQea: 0,
+    qea:[
+      {
+        question: 'What We Do',
+        answer: 'Learning Possibilities',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        list: ['We enrich lives through learning.', 'Maximizing potential through individual attention.', 'The trusted name for specialized training.', 'People teach. People learn. This is where they connect.'],
+        src: 'img/h12-tabs-icon-1.png'
+      },
+      {
+        question: 'Degree Programme',
+        answer: 'Our Students Are The Best',
+        text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        list: ['Maximizing potential through individual attention.', 'We enrich lives through learning.', 'People teach. People learn. This is where they connect.', 'The trusted name for specialized training.'],
+        src: 'img/h12-tabs-icon-2.png'
+      },
+      {
+        question: 'Career Achievements',
+        answer: 'Works After School',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        list: ['We enrich lives through learning.', 'Maximizing potential through individual attention.', 'The trusted name for specialized training.', 'People teach. People learn. This is where they connect.'],
+        src: 'img/h12-tabs-icon-3.png'
+      },
+      {
+        question: 'Personal Management',
+        answer: 'Your Business',
+        text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        list: ['Maximizing potential through individual attention.', 'We enrich lives through learning.', 'People teach. People learn. This is where they connect.', 'The trusted name for specialized training.'],
+        src: 'img/h12-tabs-icon-4.png'
+      },
+      {
+        question: 'Steps To Success',
+        answer: 'Study, Hard Work and Success',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        list: ['We enrich lives through learning.', 'Maximizing potential through individual attention.', 'The trusted name for specialized training.', 'People teach. People learn. This is where they connect.'],
+        src: 'img/h12-tabs-icon-5.png'
+      },
+      {
+        question: 'Knowledge Transfer',
+        answer: 'Our Mission',
+        text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        list: ['Maximizing potential through individual attention.', 'We enrich lives through learning.', 'People teach. People learn. This is where they connect.', 'The trusted name for specialized training.'],
+        src: 'img/h12-tabs-icon-6.png'
+      }
+    ],
+    counterCourses: 0,
+    courses:[
+      'img/landing-carousel-gallery-img-1.jpg',
+      'img/landing-carousel-gallery-img-2.jpg',
+      'img/landing-carousel-gallery-img-3.jpg',
+      'img/landing-carousel-gallery-img-4.jpg',
+      'img/landing-carousel-gallery-img-5.jpg',
+      'img/landing-carousel-gallery-img-7.jpg',
+      'img/landing-carousel-gallery-img-2.jpg',
+      'img/landing-carousel-gallery-img-3.jpg',
+      'img/landing-carousel-gallery-img-4.jpg'
+    ],
+    head:[
+      {
+        title: 'Save up to 40% by paying weekly',
+      },
+      {
+        src: 'img/h5-custom-icon-7.png',
+        title: 'Standard',
+        price: '$ 12',
+      },
+      {
+        src: 'img/h5-custom-icon-8.png',
+        title: 'Professional',
+        price: '$ 59',
+      },
+      {
+        src: 'img/h5-custom-icon-9.png',
+        title: 'Advanced',
+        price: '$ 88',
+      }
+    ],
+    rows: [
+      {
+        offer: 'Number of Courses',
+        std: '2',
+        pro: '4',
+        adv: '6'
+      },
+      {
+        offer: 'Time',
+        std: '15 Days',
+        pro: '30 Days',
+        adv: '30 Days'
+      },
+      {
+        offer: 'Web Designing',
+        std: 'y',
+        pro: 'y',
+        adv: 'y'
+      },
+      {
+        offer: 'Human-Centered Design',
+        std: 'y',
+        pro: 'y',
+        adv: 'y'
+      },
+      {
+        offer: 'Basic Marketing',
+        std: 'x',
+        pro: 'y',
+        adv: 'y'
+      },
+      {
+        offer: 'Python for Everybody',
+        std: 'x',
+        pro: 'y',
+        adv: 'y'
+      },
+      {
+        offer: 'Android Developer',
+        std: 'x',
+        pro: 'x',
+        adv: 'y'
+      },
+      {
+        offer: 'Business English',
+        std: 'x',
+        pro: 'x',
+        adv: 'y'
+      },
+      {
+        offer: '',
+        std: 'btn',
+        pro: 'btn',
+        adv: 'btn'
+      }
+    ],
+    client:[
+      'img/h5-client-1.png',
+      'img/h5-client-2.png',
+      'img/h5-client-3.png',
+      'img/h5-client-4.png'
     ]
+  },
+  computed: {
+    "columns": function columns() {
+      if (this.rows.length == 0) {
+        return [];
+      }
+      return Object.keys(this.rows[0])
+    }
   },
   mounted(){
   },
@@ -115,38 +262,12 @@ var app = new Vue({
     },
     changeReview(index){
       this.counterReview = index;
+    },
+    changeQuestion(index){
+      this.counterQea = index;
+    },
+    changeCourses(index){
+      this.counterCourses = index
     }
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// backdrop_path
-// genre_ids
-// id
-// title --> name-(serie)
-// original_language
-// original_title --> original_name-(serie)
-// overview
-// vote_average
-// poster_path
-// popularity
-// vote_count
